09769ee626fde0088a221330e752407e2ac5a718
keita8 <abdulkabirkeita825@gmail.com>
2023-02-13 08:28:21 +0100

    menu hamburger added

---
+++ .history/js/ecommerce_20230213020606.js
@@ -0,0 +1,88 @@
+const searchTogglers = document.querySelectorAll('[data-search-toggler]');
+const searchBox = document.querySelector('[data-search-box]');
+
+
+
+for (let i = 0; i < searchTogglers.length; i++) {
+    searchTogglers[i].addEventListener('click', function() {
+        searchBox.classList.toggle('active');
+    })
+
+}
+
+
+
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+    const mobileMenuCloseFunc = function() {
+        mobileMenu[i].classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    mobileMenuOpenBtn[i].addEventListener('click', function() {
+        mobileMenu[i].classList.add('active');
+        overlay.classList.add('active');
+    })
+
+    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+    overlay.addEventListener('click', mobileMenuCloseFunc);
+
+}
+
+
+//accorionBtn
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+    accordionBtn[i].addEventListener('click', function() {
+        const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+        for (let i = 0; i < accordion.length; i++) {
+            if (clickedBtn) break;
+            if (accordion[i].classList.contains('active')) {
+                accordion[i].classList.remove('active');
+                accordionBtn[i].classList.remove('active');
+            }
+        }
+
+        this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+    })
+
+}
+
+
+// slider
+
+const swiper = new Swiper('.sliderbox', {
+    // Optional parameters
+    loop: true,
+    effect: 'creative',
+
+    autoHeight: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+        delay: 5000,
+    },
+});
